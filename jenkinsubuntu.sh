6d591e8aa3aa807a37c1a6098c4b9ab9d91d1b48
Akshaykodihalli <abhayaakshay23@gmail.com>
2025-04-16 19:38:50 +0530

    Update jenkinsubuntu.sh

--- jenkinsubuntu.sh
+++ jenkinsubuntu.sh
@@ -10,12 +10,12 @@ sudo apt install -y openjdk-21-jdk
 
 
 echo "adding the jenkins to the apt list....."
-
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
+
 
   echo "updating and installing the jenkins"
 
